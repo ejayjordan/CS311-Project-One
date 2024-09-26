dab54252894d007aadee88abaa178aca9d228d3b
jay <emma.jordan@bsu.edu>
2024-09-26 17:44:13 -0400

    finalized header and navBar

--- header/header.js
+++ header/header.js
@@ -1,7 +1,7 @@
-fetch('/navBar/navBar.html')
+fetch('/header/header.html')
 .then(res => res.text())
 .then(text => {
-    let oldelem = document.querySelector("script#add_navBar");
+    let oldelem = document.querySelector("script#add_header");
     let newelem = document.createElement("div");
     newelem.innerHTML = text;
     oldelem.parentNode.replaceChild(newelem,oldelem);
